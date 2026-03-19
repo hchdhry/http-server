408b0403a42e7c67647478e03875771056bacda3
unknown <hchoudhury20@hotmail.com>
2026-03-19 14:35:18 +0000

    add request parsing

--- untitled/src/Server.java
+++ untitled/src/Server.java
@@ -10,28 +10,41 @@ public class Server {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
-    private BufferedReader in;
-
+    private String method;
+    private String path;
+    private String version;
+    private String message;
     public void start(int port) throws IOException {
 
         serverSocket = new ServerSocket(port);
+        message = "Hello world";
+
         while (true) {
             clientSocket = serverSocket.accept();
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
-            out.println("HTTP/1.1 200 OK");
-            out.println("Content-Type: text/plain");
-            out.println("Content-Length: 11");
-            out.println();
-            out.println("Hello World");
-
-
             System.out.println("connected");
             String line;
             System.out.println("---- RAW REQUEST START ----");
             while ((line = in.readLine()) != null && !line.isEmpty()) {
+                if (line.startsWith("GET") || line.startsWith("POST") ||line.startsWith("PUT") || line.startsWith("DELETE")){
+//                    for (int i = 0;i<= line.length();i++){
+//                        if(line[i]==" "){}
+//
+//                    }
+                    String[] temp = line.split(" ",3);
+                    method = temp[0];
+                    path = temp[1];
+                    version = temp[2];
+                    System.out.println("yee: "+ method+path+version);
+                }
                 System.out.println(line);
             }
+            out.println(message);
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Type: text/plain");
+            out.println("Content-Length: " + message.length());
+            out.println();
             clientSocket.close();
         }
     }
