71f22621b165197838d492fce52372b4e35cdae9
unknown <hchoudhury20@hotmail.com>
2026-04-04 15:52:20 +0100

    refactor to add handle client method

--- untitled/src/Server.java
+++ untitled/src/Server.java
@@ -14,35 +14,40 @@ public class Server {
     private String path;
     private String version;
     private String message;
+
     public void start(int port) throws IOException {
 
         serverSocket = new ServerSocket(port);
         message = "Hello world";
-
         while (true) {
-            clientSocket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            System.out.println("connected");
-
-            String[] requestLine = in.readLine().split(" ",3);
-            method = requestLine[0];
-            path = requestLine[1];
-            version = requestLine[2];
-            System.out.println("yee: "+ method+path+version);
-
-            String line;
-            System.out.println("---- RAW REQUEST START ----");
-            while ((line = in.readLine()) != null && !line.isEmpty()) {
-                System.out.println(line);
-            }
-            out.println(message);
-            out.println("HTTP/1.1 200 OK");
-            out.println("Content-Type: text/plain");
-            out.println("Content-Length:" + message.length());
-            out.println();
-            clientSocket.close();
+            handleClient(serverSocket.accept());
         }
+
     }
 
+    private void handleClient(final Socket socket) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+        System.out.println("connected");
+
+        String[] requestLine = in.readLine().split(" ", 3);
+        method = requestLine[0];
+        path = requestLine[1];
+        version = requestLine[2];
+        System.out.println("yee: " + method + path + version);
+
+        String line;
+        System.out.println("---- RAW REQUEST START ----");
+        while ((line = in.readLine()) != null && !line.isEmpty()) {
+            System.out.println(line);
+        }
+
+        out.println("HTTP/1.1 200 OK");
+        out.println("Content-Type: text/plain");
+        out.println("Content-Length: " + message.length());
+        out.println();
+        out.println(message);
+        socket.close();
+
+    }
 }
